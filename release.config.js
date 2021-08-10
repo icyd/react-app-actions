0334feffc24e930fd90ac7a03d2f82eca4312c8d
Alberto Vázquez <beto.v25@gmail.com>
2021-08-09 21:12:04 -0500

    fix: corrected syntax of release.config.js file

--- release.config.js
+++ release.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  branches: "master",
-  repositoryUrl: "https://github.com/icyd/react-app-actions",
+  branches: 'master',
+  repositoryUrl: 'https://github.com/icyd/react-app-actions',
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
-      assets: [
-        { path: "build.zip", label: "Build" },
-        { path: "coverage.zip", label: "Coverage" },
-      ]
-    }],
-  ]
-}
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {path: 'build.zip', label: 'Build'},
+          {path: 'coverage.zip', label: 'Coverage'},
+        ],
+      },
+    ],
+  ],
+};
