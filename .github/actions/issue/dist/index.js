83a515080b1d6fe66a89a0a9e9cf4c3f4d9a83e6
Alberto Vázquez <beto.v25@gmail.com>
2021-08-11 18:51:32 -0500

    ci: make issue create function await

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3716,23 +3716,26 @@ exports.getState = getState;
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-try {
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
-  const octokit = new github.getOctokit(token);
-  const response = octokit.rest.issues.create({
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split('\n') : undefined,
-  });
-  core.setOutput('issue', JSON.stringify(response.data));
+(async function() {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
+    const octokit = new github.getOctokit(token);
+    const response = await octokit.rest.issues.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined,
+    });
+    core.setOutput('issue', JSON.stringify(response.data));
 
-} catch (error) {
-  core.setFailed(error.message);
-}
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
+
 
 
 /***/ }),
